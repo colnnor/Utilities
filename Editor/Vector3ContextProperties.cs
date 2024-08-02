72bc403da1d9c23b9b7ad00743b453d2625c4c62
colnnor <135847463+colnnor@users.noreply.github.com>
2024-08-02 12:48:14 -0400

    Add files via upload

---
+++ Editor/Vector3ContextProperties.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class Vector3ContextProperties
+{
+    [InitializeOnLoadMethod]
+    public static void Init()
+    {
+        EditorApplication.contextualPropertyMenu += OnPropertyContextMenu;
+    }
+    
+    private static void OnPropertyContextMenu(GenericMenu menu, SerializedProperty property)
+    {
+        if(property.propertyType != SerializedPropertyType.Vector3) return;
+        
+        menu.AddItem(new GUIContent("Zero"), false, () =>
+        {
+            property.vector3Value = Vector3.zero;
+            property.serializedObject.ApplyModifiedProperties();
+        });
+        
+    }
+}
