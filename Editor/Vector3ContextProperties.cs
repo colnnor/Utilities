ed74d0bc2e31061d874eba7e4eee46db7e24a6a2
colnnor <connorjagielski@gmail.com>
2025-09-03 11:56:21 -0400

    Adds context properties

---
+++ Editor/Vector3ContextProperties.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class Vector3ContextProperties
+{
+    [InitializeOnLoadMethod]
+    private static void Initialize()
+    {
+        // Register the context menu item for Vector3 fields
+        EditorApplication.contextualPropertyMenu += OnContextualPropertyMenu;
+    }
+    
+    static void OnContextualPropertyMenu(GenericMenu menu, SerializedProperty property)
+    {
+        if (property.propertyType != SerializedPropertyType.Vector3) return;
+        
+        menu.AddItem(new GUIContent("Zero"), false, () =>
+        {
+            property.vector3Value = Vector3.zero;
+            property.serializedObject.ApplyModifiedProperties();
+        });
+    }
+}
+
+public static class ObjectContextProperties
+{
+    [InitializeOnLoadMethod]
+    private static void Initialize()
+    {
+        // Register the context menu item for RenderTexture fields
+        EditorApplication.contextualPropertyMenu += OnContextualPropertyMenu;
+    }
+    
+    static void OnContextualPropertyMenu(GenericMenu menu, SerializedProperty property)
+    {
+        if (property.propertyType != SerializedPropertyType.ObjectReference) return;
+        menu.AddItem(new GUIContent("Set To Null"), false, () =>
+        {
+            property.objectReferenceValue = null;
+            property.serializedObject.ApplyModifiedProperties();
+        });
+    }
+}
